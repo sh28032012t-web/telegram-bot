1d229b414536d08052c2208fac2b479d27a1de5c
Admin <sh28032012t@email.com>
2026-08-27 23:33:06 +1000

    update bot

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ async def handle_start(message: Message):
 
 @dp.message()
 async def handle_message(message: Message):
-    if message.text == "Этот бот мой раб":
+    if message.text == "Это мой бот":
         await message.answer("Я бот, который выполняет команды 🙂")
 
 
